7a820357fb01f1bb7ffea7a3e6ccb5b01f28e229
huanillo <102235967+huanillo@users.noreply.github.com>
2025-03-05 21:15:43 +0100

    First part of exercise 04. Big deal pending. Hint: need to use "for each" on the arguments array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arg) {
+    var position = arg - 1;
+    array.splice(position, 1);
+    return array;
 };
 
 // Do not edit below this line
