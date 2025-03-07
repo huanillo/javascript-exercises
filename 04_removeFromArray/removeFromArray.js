d8c3704027ca4202204aad14c156847cbcadfbc0
huanillo <102235967+huanillo@users.noreply.github.com>
2025-03-07 15:19:58 +0100

    Ejercise 4 completed with 2 possible solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,21 @@
-const removeFromArray = function(array, arg) {
-    var position = arg - 1;
-    array.splice(position, 1);
-    return array;
+const removeFromArray = function(array, ...theargs) {
+    return array.filter(element => !theargs.includes(element));
 };
+
+/* THIS FOR LOOP DOES THE SAME AS THE FUNCTION ABOVE:
+
+const removeFromArray = function(array, ...theargs) {
+    let result = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        if (!theargs.includes(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    
+    return result;
+};
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
