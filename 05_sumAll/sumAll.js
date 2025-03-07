1c1ae5bb2e00e4056f2561f964fa027952ff1b54
huanillo <102235967+huanillo@users.noreply.github.com>
2025-03-07 17:31:38 +0100

    Exercise 05 made and commented

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+let sum = 0;
+let initial, final;
+if ((Math.sign(num1) === -1) || (Math.sign(num2) === -1) || //check if positive
+    (Number.isInteger(num1) === false) || (Number.isInteger(num2) === false)){ //check if integer
+    return "ERROR";
+}
+if (num1 > num2) {
+    initial = num2;
+    final = num1;
+}
+else{
+    initial = num1;
+    final = num2;
+}
+for (let i = initial; i <= final; i ++){ //ends when num1 = num2, included
+        sum += i; //adds i in every interaction of the loop
+}
+return sum;
 
 };
 
