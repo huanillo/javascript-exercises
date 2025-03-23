7f3676bbe41ed2fb3ad2269afce8f1457f20b951
huanillo <102235967+huanillo@users.noreply.github.com>
2025-03-23 15:51:19 +0100

    Exercise 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    var punctuation = /[\s\.,?!]/g;
+    var newText = text.replace(punctuation, "").toLowerCase();
 
+    return newText === newText.split("").reverse().join("");
 };
 
 // Do not edit below this line
