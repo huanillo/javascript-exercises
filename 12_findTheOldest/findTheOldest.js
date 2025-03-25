75a605a1eaa8fc2a53941e1ff5022ed2b25e7754
huanillo <102235967+huanillo@users.noreply.github.com>
2025-03-25 22:39:54 +0100

    finished last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort(function(a, b) {
+        const lastPersonAge = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+        const nextPersonAge = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+        return nextPersonAge - lastPersonAge; // Ordered from oldest to younger
+    })[0]; // Take the first one, the oldest person
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
